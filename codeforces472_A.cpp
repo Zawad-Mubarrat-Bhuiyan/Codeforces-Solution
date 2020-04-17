91e9b66ebfad7e61394707611b6a70dfa1a82bf8
Zawad-Mubarrat-Bhuiyan <52099221+Zawad-Mubarrat-Bhuiyan@users.noreply.github.com>
2020-04-17 23:27:45 +0600

    Create codeforces472_A.cpp

---
+++ codeforces472_A.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+
+    if(n%2 !=0)
+    {
+        cout<< n-9 <<" 9"<<endl;
+    }
+    else
+    {
+        cout<<n-4<<" 4"<<endl;
+    }
+    return 0;
+}
