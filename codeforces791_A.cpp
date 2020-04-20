3c3d4b9ffa672149644c647c1991d0d53179b095
Zawad-Mubarrat-Bhuiyan <52099221+Zawad-Mubarrat-Bhuiyan@users.noreply.github.com>
2020-04-20 23:03:16 +0600

    Create codeforces791_A.cpp

---
+++ codeforces791_A.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a,b;
+
+  while(cin>>a>>b)
+  {
+    int cnt = 0;
+    while(a<=b)
+    {
+      a *= 3;
+      b *= 2;
+      cnt++;
+    }
+    cout<< cnt <<endl;
+  }
+  return 0;
+}
