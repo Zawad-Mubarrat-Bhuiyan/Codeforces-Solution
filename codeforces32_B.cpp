d411d426d96478562cbfd6a01adfa83cbf2828bc
Zawad_IIUC <52099221+Zawad-Mubarrat-Bhuiyan@users.noreply.github.com>
2020-05-22 18:21:27 +0600

    Create codeforces32_B.cpp

---
+++ codeforces32_B.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  string s;
+  cin>>s;
+  for(int i=0; i<s.size(); i++)
+  {
+    if(s[i] == '.')
+    {
+      cout<<"0";
+      continue;
+    }
+    else if(s[i] == '-' && s[i+1] == '.')
+    {
+      cout<<"1";
+      i++;
+      continue;
+    }
+    else if(s[i] == '-' && s[i+1] == '-')
+    {
+        cout<<"2";
+        i++;
+        continue;
+    }
+  }
+  return 0;
+}
